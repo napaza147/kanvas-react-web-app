2ed09895390a1291f3378140534ebfbaa05ee590
napaza147 <napaza147@live.com>
2024-10-09 15:10:51 -0400

    new branch

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -2,6 +2,24 @@ export default function Lab3() {
     return (
       <div>
         <h2>Lab 3</h2>
+
+
+        <style>h1 { background-color: blue }</style>
+        <h1 class="blue">Hello</h1>
+
+        <style>h1.blue { background-color: blue }</style>
+        <h1 id="blue">Hello</h1>
+
+        <style>h1#blue { background-color: blue }</style>
+        <h1 class="blue">Hello</h1>
+
+        <style>h1 blue { background-color: blue }</style>
+        <h1 style="blue">Hello</h1>
+
+
+
+
+
       </div>
     );
   }
